d08825a3f73c77f7470782b0140cd475613f6391
Renato Silva <br.renatosilva@gmail.com>
2014-09-07 09:23:10 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# ColorMake
+
+ColorMake will colorize output of Make and GCC, highlighting errors and warnings
+so you can notice them more easily. ColorMake is implemented in Bash and
+requires a terminal with support for ANSI escape sequences.
+
+## Usage
+
+Just use the script instead of make, passing the arguments as usual. You can
+also have `make` aliased to `colormake` for instance. Just make sure to not
+rename it to `make` as that would cause recursive calls.
+
+## License and copyright
+
+Copyright (c) 2014 Renato Silva.
+Licensed under the terms of the GNU GPL version 2.
---
+++ colormake.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Colored Make 2014.9.7
+# Copyright (c) 2014 Renato Silva
+# GNU GPLv2 licensed
+
+# This script will colorize output of make and GCC so it becomes easier to
+# notice the errors and warnings in the complex build output.
+
+# Enable 256 colors for MinTTY in MSYS2
+if [[ "$MSYSCON" = mintty* && "$TERM" = *256color* ]]; then
+    red="\e[38;05;9m"
+    green="\e[38;05;76m"
+    blue="\e[38;05;74m"
+    cyan="\e[0;36m"
+    purple="\e[38;05;165m"
+    yellow="\e[0;33m"
+    gray="\e[38;05;244m"
+else
+    red="\e[1;31m"
+    green="\e[1;32m"
+    blue="\e[1;34m"
+    cyan="\e[1;36m"
+    purple="\e[1;35m"
+    yellow="\e[1;33m"
+    gray="\e[1;30m"
+fi
+normal="\e[0m"
+
+# Errors, warnings, notes and compiler recipes
+error="s/^(.*error:)/$(printf $red)\\1$(printf $normal)/i"
+warning="s/^(.*warning:)/$(printf $yellow)\\1$(printf $normal)/"
+make="s/^make(\[[0-9]+\])?:/$(printf $blue)make\\1:$(printf $normal)/"
+compiler_recipe="s/^(gcc(.exe)? .*)/$(printf $gray)\\1$(printf $normal)/"
+
+if [[ $(uname -or) != 1.*Msys ]]; then
+    command make "$@" 2> >(sed -E -e "$warning" -e "$error" -e "$make" -e "$compiler_recipe") \
+                       > >(sed -E -e "$warning" -e "$error" -e "$make" -e "$compiler_recipe")
+else
+    # MinGW MSYS does not support process substitution
+    command make "$@" 2>&1 | sed -E -e "$warning" -e "$error" -e "$make" -e "$compiler_recipe"
+fi
